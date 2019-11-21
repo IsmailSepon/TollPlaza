13f2f37ad6116305fafa9c3e2f540c75da9aec91
HP <ismailhossainsepon@gmail.com>
2019-11-21 17:18:08 +0600

    Api called is Done.......

--- app/src/main/java/com/sepon/regnumtollplaza/ChorshindduActivity.java
+++ app/src/main/java/com/sepon/regnumtollplaza/ChorshindduActivity.java
@@ -13,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
@@ -33,7 +34,6 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
-import retrofit2.Response;
 
 public class ChorshindduActivity extends AppCompatActivity {
 
@@ -43,22 +43,22 @@ public class ChorshindduActivity extends AppCompatActivity {
 
     private List<Norshinddi> todayreport;// = new ArrayList<>();
 
-    private ArrayList<Norshinddi> arrayList = new ArrayList<>();
+     ArrayList<Norshinddi> arrayList;// = new ArrayList<>();
 
 
-    private ArrayList<JSONObject> Rickshaw_Van = new ArrayList<>();
-    private ArrayList<JSONObject> MotorCycle = new ArrayList<>();
-    private ArrayList<JSONObject> three_four_Wheeler = new ArrayList<>();
-    private ArrayList<JSONObject> Sedan_Car = new ArrayList<>();
-    private ArrayList<JSONObject> Wheeler = new ArrayList<>();
-    private ArrayList<JSONObject> Micro_Bus = new ArrayList<>();
-    private ArrayList<JSONObject> Mini_Bus = new ArrayList<>();
-    private ArrayList<JSONObject> Agro_Use = new ArrayList<>();
-    private ArrayList<JSONObject> Mini_Truck = new ArrayList<>();
-    private ArrayList<JSONObject> Big_Bus = new ArrayList<>();
-    private ArrayList<JSONObject> Medium_Truck = new ArrayList<>();
-    private ArrayList<JSONObject> Heavy_Truck = new ArrayList<>();
-    private ArrayList<JSONObject> Trailer_Long = new ArrayList<>();
+     ArrayList<JSONObject> Rickshaw_Van = new ArrayList<>();
+    ArrayList<JSONObject> MotorCycle = new ArrayList<>();
+    ArrayList<JSONObject> three_four_Wheeler = new ArrayList<>();
+    ArrayList<JSONObject> Sedan_Car = new ArrayList<>();
+    ArrayList<JSONObject> Wheeler = new ArrayList<>();
+    ArrayList<JSONObject> Micro_Bus = new ArrayList<>();
+    ArrayList<JSONObject> Mini_Bus = new ArrayList<>();
+    ArrayList<JSONObject> Agro_Use = new ArrayList<>();
+    ArrayList<JSONObject> Mini_Truck = new ArrayList<>();
+    ArrayList<JSONObject> Big_Bus = new ArrayList<>();
+    ArrayList<JSONObject> Medium_Truck = new ArrayList<>();
+    ArrayList<JSONObject> Heavy_Truck = new ArrayList<>();
+    ArrayList<JSONObject> Trailer_Long = new ArrayList<>();
 
     String url = "http://103.95.99.196/api/today.php";
 
@@ -69,11 +69,7 @@ public class ChorshindduActivity extends AppCompatActivity {
         setContentView(R.layout.activity_details);
 
 
-       // getCatalogiesProduct(url);
-
-      ///  calculateCar();
-
-        getdata();
+       // getDaysReport(url);
 
 
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
@@ -98,146 +94,71 @@ public class ChorshindduActivity extends AppCompatActivity {
 
 
 
-
     }
 
-    private void getdata() {
+    private void getDaysReport(String url) {
+        arrayList = new ArrayList<>();
+        todayreport = new ArrayList<>();
+        final ProgressDialog dialog = ProgressDialog.show(this, "Getting Current Report From Server", "Please wait...", true);
 
-        ProgressDialog dialog = ProgressDialog.show(this, "Getting Current Report From Server", "Please wait...", true);
-
-        IApiClient iApiClient = ApiClient.getClient().create(IApiClient.class);
-        Call<List<Norshinddi>> call =iApiClient.gettodaysreport();
-        call.enqueue(new Callback<List<Norshinddi>>() {
+        final RequestQueue requestQueue = Volley.newRequestQueue(ChorshindduActivity.this);
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url, new com.android.volley.Response.Listener<JSONArray>() {
             @Override
-            public void onResponse(Call<List<Norshinddi>> call, Response<List<Norshinddi>> response) {
-                int s = response.code();
-                Log.e("Status", String.valueOf(s));
+            public void onResponse(JSONArray response) {
+                Log.e("Reponse", String.valueOf(response.length()));
+
+                for (int i=0; i<response.length();i++){
+                    try {
+
+                        JSONObject j =  response.getJSONObject(i);
+                      //  Log.e("Amount", j.getString("amount"));
+                        Norshinddi norshinddi = new Norshinddi(j.getString("Agro_Use"),
+                               j.getString("amount"),
+                               j.getString("Big_Bus"),
+                               j.getString("date_time"),
+                               j.getString("Heavy_Truck"),
+                               j.getString("Medium_Truck"),
+                               j.getString("Micro_Bus"),
+                               j.getString("Mini_Bus"),
+                               j.getString("Mini_Truck"),
+                               j.getString("MotorCycle"),
+                               j.getString("pass_id"),
+                               j.getString("RegNO"),
+                               j.getString("Rickshaw_Van"),
+                               j.getString("Sedan_Car"),
+                               j.getString("three_four_Wheeler"),
+                               j.getString("Trailer_Long"),
+                               j.getString("4Wheeler"));
+
+                      //  Log.e("NOrshinddi", norshinddi.getAmount());
+                        //arrayList.add(norshinddi);
+                        arrayList.add(i, norshinddi);
+                        todayreport.add(norshinddi);
+                       // Log.e("Arraylist ", String.valueOf(arrayList.size()));
+
+                    }catch (Exception p){
+
+                    }
+                }
+
+                Log.e("Arraylist ", String.valueOf(arrayList.size()));
+                Log.e("List Today ", String.valueOf(todayreport.size()));
+
                 dialog.dismiss();
             }
-
+        }, new Response.ErrorListener() {
             @Override
-            public void onFailure(Call<List<Norshinddi>> call, Throwable t) {
+            public void onErrorResponse(VolleyError error) {
 
-                dialog.dismiss();
             }
         });
+        requestQueue.add(jsonArrayRequest);
 
-    }
-
-
-    public void getCatalogiesProduct(String url){
-        ProgressDialog dialog = ProgressDialog.show(this, "Getting Current Report From Server", "Please wait...", true);
-
-        // Initialize a new RequestQueue instance
-        RequestQueue requestQueue = Volley.newRequestQueue(ChorshindduActivity.this);
-
-       JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url, new com.android.volley.Response.Listener<JSONArray>() {
-           @Override
-           public void onResponse(JSONArray response) {
-               Log.e("Car: ", String.valueOf(response.length()));
-               for (int i=0; i<response.length();i++){
-                   try {
-                      // JSONObject j = response.getJSONObject(i);
-                       String o = response.getJSONObject(i).toString();
-                       JsonParser parser = new JsonParser();
-                       JsonElement jsonObject = parser.parse(o);
-                       Gson gson = new Gson();
-                       Norshinddi norshinddi = gson.fromJson(jsonObject, Norshinddi.class);
-                      // Norshinddi nor = gson.fromJson(j, Norshinddi.class);
-                      // Name name=gson.fromJson(jsonString,Name.class);
-
-//                       Norshinddi norshinddi = new Norshinddi(j.getString("Agro_Use"),
-//                               j.getString("amount"),
-//                               j.getString("Big_Bus"),
-//                               j.getString("date_time"),
-//                               j.getString("Heavy_Truck"),
-//                               j.getString("Medium_Truck"),
-//                               j.getString("Micro_Bus"),
-//                               j.getString("Mini_Bus"),
-//                               j.getString("Mini_Truck"),
-//                               j.getString("MotorCycle"),
-//                               j.getString("pass_id"),
-//                               j.getString("RegNO"),
-//                               j.getString("Rickshaw_Van"),
-//                               j.getString("Sedan_Car"),
-//                               j.getString("three_four_Wheeler"),
-//                               j.getString("Trailer_Long"),
-//                               j.getString("4Wheeler"));
-                       arrayList.add(norshinddi);
-//                       if (j.getString("Rickshaw_Van").equals("1")){
-//                           Rickshaw_Van.add(j);
-//                       }else if (j.getString("MotorCycle").equals("1")){
-//                           MotorCycle.add(j);
-//                       }else if (j.getString("three_four_Wheeler").equals("1")){
-//                           three_four_Wheeler.add(j);
-//                       }else if (j.getString("Sedan_Car").equals("1")){
-//                           Sedan_Car.add(j);
-//                       }else if (j.getString("4Wheeler").equals("1")){
-//                           Wheeler.add(j);
-//                       }else if (j.getString("Micro_Bus").equals("1")){
-//                           Micro_Bus.add(j);
-//                       }else if (j.getString("Mini_Bus").equals("1")){
-//                           Mini_Bus.add(j);
-//                       }else if (j.getString("Agro_Use").equals("1")){
-//                           Agro_Use.add(j);
-//                       }else if (j.getString("Mini_Truck").equals("1")){
-//                           Mini_Truck.add(j);
-//                       }else if (j.getString("Big_Bus").equals("1")){
-//                           Big_Bus.add(j);
-//                       }else if (j.getString("Medium_Truck").equals("1")){
-//                           Medium_Truck.add(j);
-//                       }else if (j.getString("Heavy_Truck").equals("1")){
-//                           Heavy_Truck.add(j);
-//                       }else if (j.getString("Trailer_Long").equals("1")){
-//                           Trailer_Long.add(j);
-//                       }else{
-//                           arrayList.add(j);
-//                       }
-
-
-                       //
-                   }catch (Exception e){
-
-                   }
-               }
-
-               dialog.dismiss();
-           }
-       }, new com.android.volley.Response.ErrorListener() {
-           @Override
-           public void onErrorResponse(VolleyError error) {
-
-
-               dialog.dismiss();
-           }
-       });
-       requestQueue.add(jsonArrayRequest);
+//        Log.e("Arraylist ", String.valueOf(arrayList.size()));
+//        Log.e("List Today ", String.valueOf(todayreport.size()));
 
 
     }
 
-    private void calculateCar() {
-        Log.e("Arraylist: ", String.valueOf(arrayList.size()));
-
-        for (int i=0; i<arrayList.size();i++){
-           String amouhnt = arrayList.get(i).getAmount();
-            Log.e("Amount", amouhnt);
-        }
-
-        Log.e("Rickshaw_Van", String.valueOf(arrayList.size()));
-//        Log.e(" MotorCycle", String.valueOf(MotorCycle.size()));
-//        Log.e("three_four_Wheeler ", String.valueOf(three_four_Wheeler.size()));
-//        Log.e("Sedan_Car ", String.valueOf(Sedan_Car.size()));
-//        Log.e("Wheeler ", String.valueOf(Wheeler.size()));
-//        Log.e("Micro_Bus ", String.valueOf(Micro_Bus.size()));
-//        Log.e("Mini_Bus ", String.valueOf(Mini_Bus.size()));
-//        Log.e("Agro_Use ", String.valueOf(Agro_Use.size()));
-//        Log.e("Mini_Truck ", String.valueOf(Mini_Truck.size()));
-//        Log.e("Big_Bus ", String.valueOf(Big_Bus.size()));
-//        Log.e("Medium_Truck ", String.valueOf(Medium_Truck.size()));
-//        Log.e("Mini_Truck ", String.valueOf(Mini_Truck.size()));
-//        Log.e("Heavy_Truck ", String.valueOf(Heavy_Truck.size()));
-//        Log.e("Trailer_Long ", String.valueOf(Trailer_Long.size()));
-    }
 
 }
--- app/src/main/java/com/sepon/regnumtollplaza/fragment/Today_Fragment.java
+++ app/src/main/java/com/sepon/regnumtollplaza/fragment/Today_Fragment.java
@@ -17,11 +17,14 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+import com.sepon.regnumtollplaza.ChorshindduActivity;
 import com.sepon.regnumtollplaza.R;
 import com.sepon.regnumtollplaza.adapter.TodayAdapter;
+import com.sepon.regnumtollplaza.pojo.Norshinddi;
 import com.sepon.regnumtollplaza.pojo.Tali;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,6 +37,11 @@ public class Today_Fragment extends Fragment {
     TodayAdapter taliAdapter;
     RecyclerView recyclerView;
     String url = "http://103.95.99.196/api/today.php";
+
+    private List<Norshinddi> todayreport;// = new ArrayList<>();
+
+    ArrayList<Norshinddi> arrayList;// = new ArrayList<>();
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -61,28 +69,150 @@ public class Today_Fragment extends Fragment {
         recyclerView.setAdapter(taliAdapter);
 
 
-        ProgressDialog dialog = ProgressDialog.show(getActivity(), "Getting Current Report From Server", "Please wait...", true);
+        getDaysReport(url);
 
-        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new  Response.Listener<JSONArray>() {
+        return view;
+    }
+
+
+    private void getDaysReport(String url) {
+     //   arrayList = new ArrayList<>();
+        todayreport = new ArrayList<>();
+        final ProgressDialog dialog = ProgressDialog.show(getActivity(), "Getting Current Report From Server", "Please wait...", true);
+
+        final RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url, new com.android.volley.Response.Listener<JSONArray>() {
             @Override
             public void onResponse(JSONArray response) {
-                if (response.length()>0){
-                    for (int i=0; i<response.length();i++){
-                        Log.e("new", String.valueOf(i));
+                Log.e("Reponse", String.valueOf(response.length()));
+
+                for (int i=0; i<response.length();i++){
+                    try {
+
+                        JSONObject j =  response.getJSONObject(i);
+                        //  Log.e("Amount", j.getString("amount"));
+                        Norshinddi norshinddi = new Norshinddi(j.getString("Agro_Use"),
+                                j.getString("amount"),
+                                j.getString("Big_Bus"),
+                                j.getString("date_time"),
+                                j.getString("Heavy_Truck"),
+                                j.getString("Medium_Truck"),
+                                j.getString("Micro_Bus"),
+                                j.getString("Mini_Bus"),
+                                j.getString("Mini_Truck"),
+                                j.getString("MotorCycle"),
+                                j.getString("pass_id"),
+                                j.getString("RegNO"),
+                                j.getString("Rickshaw_Van"),
+                                j.getString("Sedan_Car"),
+                                j.getString("three_four_Wheeler"),
+                                j.getString("Trailer_Long"),
+                                j.getString("4Wheeler"));
+                        todayreport.add(norshinddi);
+
+                    }catch (Exception p){
+
                     }
                 }
 
+             //   Log.e("List Today ", String.valueOf(todayreport.size()));
+                serializeData(todayreport);
                 dialog.dismiss();
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                dialog.dismiss();
+
             }
         });
         requestQueue.add(jsonArrayRequest);
-        return view;
+    }
+
+    private void serializeData(List<Norshinddi> todayreport) {
+        final ProgressDialog dialog = ProgressDialog.show(getActivity(), "Serialize Data ", "Please wait...", true);
+
+        ArrayList<Norshinddi> Rickshaw_Van = new ArrayList<>();
+        ArrayList<Norshinddi> MotorCycle = new ArrayList<>();
+        ArrayList<Norshinddi> three_four_Wheeler = new ArrayList<>();
+        ArrayList<Norshinddi> Sedan_Car = new ArrayList<>();
+        ArrayList<Norshinddi> Wheeler = new ArrayList<>();
+        ArrayList<Norshinddi> Micro_Bus = new ArrayList<>();
+        ArrayList<Norshinddi> Mini_Bus = new ArrayList<>();
+        ArrayList<Norshinddi> Agro_Use = new ArrayList<>();
+        ArrayList<Norshinddi> Mini_Truck = new ArrayList<>();
+        ArrayList<Norshinddi> Big_Bus = new ArrayList<>();
+        ArrayList<Norshinddi> Medium_Truck = new ArrayList<>();
+        ArrayList<Norshinddi> Heavy_Truck = new ArrayList<>();
+        ArrayList<Norshinddi> Trailer_Long = new ArrayList<>();
+        ArrayList<Norshinddi> VIP = new ArrayList<>();
+
+
+
+        Log.e("LIst :  ", String.valueOf(todayreport.size()));
+        for (int i=0; i<todayreport.size();i++){
+
+            if (todayreport.get(i).getMicroBus().equals("1")){
+                Micro_Bus.add(todayreport.get(i));
+
+            }else if (todayreport.get(i).getAgroUse().equals("1")){
+                Agro_Use.add(todayreport.get(i));
+
+            }else if (todayreport.get(i).getBigBus().equals("1")){
+                Big_Bus.add(todayreport.get(i));
+
+            }else if (todayreport.get(i).getHeavyTruck().equals("1")){
+                Heavy_Truck.add(todayreport.get(i));
+
+            }else if (todayreport.get(i).getMediumTruck().equals("1")){
+                Medium_Truck.add(todayreport.get(i));
+
+            }else if (todayreport.get(i).getMiniBus().equals("1")){
+                Mini_Bus.add(todayreport.get(i));
+
+            }else if (todayreport.get(i).getMiniTruck().equals("1")){
+                Mini_Truck.add(todayreport.get(i));
+
+            }else if (todayreport.get(i).getMotorCycle().equals("1")){
+                MotorCycle.add(todayreport.get(i));
+
+            }else if (todayreport.get(i).getRickshawVan().equals("1")){
+                Rickshaw_Van.add(todayreport.get(i));
+
+            }else if (todayreport.get(i).getSedanCar().equals("1")){
+                Sedan_Car.add(todayreport.get(i));
+
+            }else if (todayreport.get(i).getThreeFourWheeler().equals("1")){
+                three_four_Wheeler.add(todayreport.get(i));
+
+            }else if (todayreport.get(i).getTrailerLong().equals("1")){
+                Trailer_Long.add(todayreport.get(i));
+
+            }else if (todayreport.get(i).getWheeler().equals("1")){
+                Wheeler.add(todayreport.get(i));
+
+            }else {
+                VIP.add(todayreport.get(i));
+              //  Log.e("NUll ","error" );
+            }
+        }
+
+
+        Log.e("Rickshaw_Van", String.valueOf(Rickshaw_Van.size()));
+        Log.e(" MotorCycle", String.valueOf(MotorCycle.size()));
+        Log.e("Wheeler ", String.valueOf(Wheeler.size()));
+        Log.e("Micro_Bus ", String.valueOf(Micro_Bus.size()));
+        Log.e("Mini_Bus ", String.valueOf(Mini_Bus.size()));
+        Log.e("Agro_Use ", String.valueOf(Agro_Use.size()));
+        Log.e("Mini_Truck ", String.valueOf(Mini_Truck.size()));
+        Log.e("Big_Bus ", String.valueOf(Big_Bus.size()));
+        Log.e("three_four_Wheeler ", String.valueOf(three_four_Wheeler.size()));
+        Log.e("Sedan_Car ", String.valueOf(Sedan_Car.size()));
+        Log.e("Medium_Truck ", String.valueOf(Medium_Truck.size()));
+        Log.e("Mini_Truck ", String.valueOf(Mini_Truck.size()));
+        Log.e("Heavy_Truck ", String.valueOf(Heavy_Truck.size()));
+        Log.e("Trailer_Long ", String.valueOf(Trailer_Long.size()));
+        Log.e("Trailer_Long ", String.valueOf(VIP.size()));
+        dialog.dismiss();
     }
 
 
